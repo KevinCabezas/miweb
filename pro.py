10c22125fefeb96813f00428d33dc0849b78697b
Kevin Cabezas <angelescabesuchi@gmail.com>
2023-12-27 00:00:21 -0300

    actualizar codigo

--- pro.py
+++ pro.py
@@ -1,13 +1,15 @@
-mensaje = "hola mundo"
-# print(mensaje)
-contador = 0
+def descuento_del_10_porciento(precio):
 
-for letra in mensaje:
-    if (letra == " "):
-        contador -= 1
-    contador +=1
+    descuento = 30
+    cuenta = precio - ((precio * descuento) / 100)
+    return cuenta
 
-print(contador)
-print(contador)
+def agragrar_iva(precio):
 
+    aumento = 21
+    cuenta = precio + ((precio * aumento) / 100)
+    return cuenta
 
+ingresar_precio = float(input("ingrese el precio: "))
+mensaje = "Precio con el 30% de descuento: {0}".format(descuento_del_10_porciento(ingresar_precio))
+print(mensaje)
